72aa1d3153bc7430032dd134ce2bae477812fc46
Gerrit DeRyke <gderyke@gmail.com>
2021-05-21 01:41:43 -0500

    all but stars loop

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -40,6 +40,9 @@ for (let i=2; i<11; i+=2) {
 // 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 
+for (let i=5; i>=0; i--){
+  console.log(i);
+}
 
 // 2. For of loops
 console.log('---- 2. For Of loop ----');
@@ -48,6 +51,9 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
 
+for (name of stars){
+  console.log(name);
+}
 
 // 3. While loops
 console.log('---- 3. While loop ----');
@@ -55,8 +61,26 @@ console.log('---- 3. While loop ----');
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
 
+
+
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
 
+let i = 0
+const max = 6
+
+while(i < max){
+  console.log(i);
+  i++
+}
+
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+
+let index = 10
+const min = 5
+
+while(index >= min){
+  console.log(index);
+  index--
+}
